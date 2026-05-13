c917c9b1d883ce0a88267ddd1279deb097e72375
Paulo Vinicius <117373359+PauloViniciusAF@users.noreply.github.com>
2026-05-13 15:01:04 -0300

    Fix spelling FutSite Footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
     <footer className="footer">
       <div className="footer-content">
         <a 
-          href="https://github.com/PauloViniciusAF/CC6240-DaChamp" 
+          href="https://github.com/PauloViniciusAF/CC6240-FutSite" 
           target="_blank" 
           rel="noopener noreferrer"
           className="footer-link"
